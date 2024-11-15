73ad446d57eccd7436b31c6fc3b72882b270f2b1
le factuer <vxlkhoi@gmail.com>
2024-11-15 13:37:08 +0000

    đổi màu thì chuẩn rồi nhưng mà footer đang hơi ngu, chờ sửa tí

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,18 @@ tabs.forEach((tab) => {
     tab.classList.add("active-tab");
   });
 });
+// fx: đổi màu nền khi cuộn
+$(function () {
+  'use strict';
+  var view = $(window).height();
+  $('.part')
+      .height(view)
+      .scrollie({
+          scrollOffset: -50,
+          scrollingInView: function (elem) {
+              var bgColor = elem.data('background');
+              $('body').css('background-color', bgColor);
+          }
+      });
+});
+// fx: đổi màu nền khi cuộn
---
+++ assets/js/scrollie.js
@@ -0,0 +1,28 @@
+/*--- 
+
+Author: YasirKareem
+Channel: Design & Code
+Link: https://www.youtube.com/DesignCodeY
+Date: Feb 2, 2019
+If you like the lesson please subscribe to the channel
+
+---*/
+
+/*global $*/
+
+// fx: đổi màu nền khi cuộn
+$(function () {
+    'use strict';
+    var view = $(window).height();
+    $('.part')
+        .height(view)
+        .scrollie({
+            scrollOffset: -50,
+            scrollingInView: function (elem) {
+                var bgColor = elem.data('background');
+                $('body').css('background-color', bgColor);
+            }
+        });
+  });
+  // fx: đổi màu nền khi cuộn
+  
